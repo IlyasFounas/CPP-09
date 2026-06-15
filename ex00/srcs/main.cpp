dd912a9941391bf74b6d94e9c71b71fbbd0ec0cd
Ilyas Founas <ifounas@student.42lyon.fr>
2026-06-15 11:53:13 +0200

    try to fix ex00

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -1,6 +1,6 @@
 #include "BitcoinExchange.hpp"
 
-void basic_process(BitcoinExchange &btc_program, const std::string &file_name)
+int basic_process(BitcoinExchange &btc_program, const std::string &file_name)
 {
     btc_program.setFileName(file_name);
     try
@@ -10,18 +10,21 @@ void basic_process(BitcoinExchange &btc_program, const std::string &file_name)
     }
     catch(const std::exception& e)
     {
-        std::cout << e.what() << std::endl;
-    }    
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+    return 0;   
 }
 
 int main(int argc, char **argv)
 {
     BitcoinExchange btc_program;
+    int state = 0;
     if (argc < 2 || argc > 2)
     {
-        std::cout << "Error no file provided." << std::endl;
+        std::cerr << "Error no file provided." << std::endl;
         return (1);
     }
-    basic_process(btc_program, argv[1]);
-    return (0);
+    state = basic_process(btc_program, argv[1]);
+    return (state);
 }
